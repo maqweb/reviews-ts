605b98b211797309c997830b551e1d86a54adf76
Vitaly Balikhin <balikhinv@bk.ru>
2023-02-11 00:04:34 +0300

    title component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
+import React from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
-	return <Component {...pageProps} />;
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+	return <>
+		<Head>
+			<title>My Next App</title>
+			<link rel="icon" href="/favicon.ico" />
+			<link rel="preconnect" href="https://fonts.google.com" />
+			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" />
+		</Head>
+		<Component {...pageProps} />
+	</>;
 }
+
+export default MyApp;
