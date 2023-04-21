352de7d2ef1b5a265ad580eb6605053063737bd5
Vitaly Balikhin <balikhinv@bk.ru>
2023-04-22 00:48:22 +0300

    added rating component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Htag, Ptag, Tag } from '../components';
+import { Rating } from '../components/Rating/Rating';
 
 export default function Home(): JSX.Element {
+	const [rating, setRating] = useState<number>(4);
+
 
 	return (
 		<>
-			<Htag tag='h2'>Text</Htag>
-			<Button appearance='primary'>Кнопка</Button>
-			<Button appearance='ghost' arrow='right'>Кнопка</Button>
+			<Htag tag='h2'>Title</Htag>
+			<Button appearance='primary' arrow='right'>Кнопка</Button>
+			<Button appearance='ghost' arrow='down'>Кнопка</Button>
 
 			<Ptag size='s'>Текст</Ptag>
 			<Ptag size='m'>Текст</Ptag>
@@ -17,6 +20,8 @@ export default function Home(): JSX.Element {
 			<Tag color='primary' size='s'>Текст</Tag>
 			<Tag color='red' size='m'>Текст</Tag>
 			<Tag color='ghost' size='s'>Текст</Tag>
+
+			<Rating rating={rating} isEditable setRating={setRating} />
 		</>
 	);
 }
